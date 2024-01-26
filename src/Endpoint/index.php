af39ff1ab339c91dd2d0e73b03eeb416de9af446
Scott Joudry <sj@slydevil.com>
2024-01-26 09:28:52 -0400

    Issue #3: do it the wordpress way.

--- src/Endpoint/index.php
+++ src/Endpoint/index.php
@@ -1,8 +1,2 @@
 <?php
-
-// Prevent direct execution of this file.
-if (!function_exists('add_filter')) {
-  header('Status: 403 Forbidden');
-  header('HTTP/1.1 403 Forbidden');
-	exit();
-}
+// silence is golden.
